4eacf27d59902a987355796f52653787141eb43e
Professor MAD <professormad@proton.me>
2024-05-26 16:01:33 -0400

    Made comment container and footer fucking work

--- src/App.js
+++ src/App.js
@@ -6,11 +6,20 @@ const App = () => {
   return (
     <div className="app-container">
       <h1 className="my-day">MY DAY</h1>
+
       <div className='date-wrap'>
         <h3 className="date day">Sunday</h3>
         <h3 className='date-numbers'>26</h3>
         <h3 className='date'>May</h3>
       </div>
+
+      <div className='todo-content-container'>
+        Todo content goes here
+      </div>
+
+      <footer className='footer-container'>
+        This is a footer
+      </footer>
     </div>
   );
 };
