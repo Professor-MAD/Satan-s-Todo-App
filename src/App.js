9f5082251fe17e8d466088423cbd99e1454b4437
Professor MAD <professormad@proton.me>
2024-06-05 21:32:28 -0400

    api key remove

--- src/App.js
+++ src/App.js
@@ -9,27 +9,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ThemeSwitcher from './components/ThemeSwitcher/ThemeSwitcher';
 import Slideshow from './components/Slideshow/Slideshow';
 import { ThemeProvider } from './components/ThemeContext/ThemeContext';
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDVqJ7HttoIDV7Jdd-yRaZ-c3R5nSGIK6E",
-//   authDomain: "satanic-agenda.firebaseapp.com",
-//   projectId: "satanic-agenda",
-//   storageBucket: "satanic-agenda.appspot.com",
-//   messagingSenderId: "1056746325497",
-//   appId: "1:1056746325497:web:fecd3c5309feacedfb2928",
-//   measurementId: "G-PMJJ2QQKJE"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
 
 const App = () => {
   const [todos, setTodos] = useState(() => {
